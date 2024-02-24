05beb79f782345c0ad8b818524406a6c5a837683
Michael-Safwat <mikesafwat34@gmail.com>
2024-02-24 18:16:27 +0200

    add cluster map

--- models/campground.js
+++ models/campground.js
@@ -11,6 +11,8 @@ ImageSchema.virtual('thumbnail').get(function () {
   return this.url.replace('/upload', '/upload/w_200');
 });
 
+const opts = { toJSON: { virtuals: true } };
+
 const campgroundSchema = new schema({
   title: String,
   images: [ImageSchema],
@@ -38,6 +40,12 @@ const campgroundSchema = new schema({
           ref: 'Review'
       }
   ]
+},opts);
+
+campgroundSchema.virtual('properties.popUpMarkup').get(function () {
+  return `
+  <strong><a href="/campgrounds/${this._id}">${this.title}</a><strong>
+  <p>${this.description.substring(0, 20)}...</p>`
 });
 
 campgroundSchema.post('findOneAndDelete', async function (doc) {
